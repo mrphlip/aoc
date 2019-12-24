76abfd0fae08215c0b1be4566967a05b3ec7eb66
phlip <git@mrphlip.com>
2019-12-24 19:20:23 +1100

    Rework 22 so everything is using the Monoid-ish stuff

--- 22.hs
+++ 22.hs
@@ -2,8 +2,35 @@
 import Utils
 import Control.Exception
 
-data Operation = DealNew | DealIncrement Integer | Cut Integer deriving (Eq, Show, Read)
-type Deck = (Integer, Integer, Integer) -- size, init, offset
+-- init, offset
+type Operation = (Integer, Integer)
+
+-- this really feels like it should be a Monoid, with
+-- (mempty, mappend, mconcat) = (noop, addOperations size, combineOperations size)
+-- but having to pass that extra "size" value around makes it real awkward
+noop = (0,1) :: Operation
+
+addOperations :: Integer -> Operation -> Operation -> Operation
+addOperations size (init1, ofs1) (init2, ofs2) = ((init2 + init1 * ofs2) `mod` size, (ofs1 * ofs2) `mod` size)
+
+combineOperations :: Integer -> [Operation] -> Operation
+combineOperations size = foldr (addOperations size) noop
+
+-- Lifting the implementation of (^) from Prelude
+multiplyOperations :: Integer -> Operation -> Integer -> Operation
+multiplyOperations size op n
+	| n < 0 = error "Negative exponent"
+	| n == 0 = noop
+	| otherwise = f op n
+	where
+		f op n
+			| even n = f (addOperations size op op) (n `quot` 2)
+			| n == 1 = op
+			| otherwise = g (addOperations size op op) (n `quot` 2) op
+		g op n x
+			| even n = g (addOperations size op op) (n `quot` 2) x
+			| n == 1 = addOperations size op x
+			| otherwise = g (addOperations size op op) (n `quot` 2) (addOperations size op x)
 
 getInput :: IO [Operation]
 getInput = do
@@ -14,55 +41,30 @@ parseInput :: String -> [Operation]
 parseInput = map parseLine . lines
 
 parseLine :: String -> Operation
-parseLine "deal into new stack" = DealNew
-parseLine ('d':'e':'a':'l':' ':'w':'i':'t':'h':' ':'i':'n':'c':'r':'e':'m':'e':'n':'t':' ':rest) = DealIncrement $ read rest
-parseLine ('c':'u':'t':' ':rest) = Cut $ read rest
+parseLine "deal into new stack" = (-1, -1)
+parseLine ('d':'e':'a':'l':' ':'w':'i':'t':'h':' ':'i':'n':'c':'r':'e':'m':'e':'n':'t':' ':rest) = (0, read rest)
+parseLine ('c':'u':'t':' ':rest) = (-read rest, 1)
 
-newDeck :: Integer -> Deck
-newDeck size = (size, 0, 1)
+cardPosition :: Integer -> Operation -> Integer -> Integer
+cardPosition size (init, ofs) n = (init + ofs * n) `mod` size
 
-doOperation :: Operation -> Deck -> Deck
-doOperation DealNew (size, init, ofs) = (size, (-init - 1) `mod` size, (-ofs) `mod` size)
-doOperation (DealIncrement n) (size, init, ofs) = (size, (init * n) `mod` size, (ofs * n) `mod` size)
-doOperation (Cut n) (size, init, ofs) = (size, (init - n) `mod` size, ofs)
-
-doOperations :: [Operation] -> Deck -> Deck
-doOperations = flip $ foldl (flip doOperation)
-
-cardPosition :: Deck -> Integer -> Integer
-cardPosition (size, init, ofs) n = (init + ofs * n) `mod` size
-
-cardAt :: Deck -> Integer -> Integer
-cardAt (size, init, ofs) n = ((n - init) * modRecip ofs size) `mod` size
-
-addOperations :: Deck -> Deck -> Deck
-addOperations (size, init, ofs) deck2@(size2, _, _) = assert (size == size2) $ (size, zeropos, newofs)
-	where
-		zeropos = cardPosition deck2 init
-		onepos = cardPosition deck2 (init + ofs)
-		newofs = (onepos - zeropos) `mod` size
-
-doubleOperations deck = addOperations deck deck
-multiplyOperations (size, _, _) 0 = newDeck size
-multiplyOperations deck 1 = deck
-multiplyOperations deck n
-	| even n = multiplyOperations (doubleOperations deck) (n `div` 2)
-	| odd n = addOperations (multiplyOperations (doubleOperations deck) (n `div` 2)) deck
+cardAt :: Integer -> Operation -> Integer -> Integer
+cardAt size (init, ofs) n = ((n - init) * modRecip ofs size) `mod` size
 
 tests :: IO ()
 tests = do
-	testShuffle "deal with increment 7\ndeal into new stack\ndeal into new stack" (10,0,7)
-	testShuffle "cut 6\ndeal with increment 7\ndeal into new stack" (10,1,3)
-	testShuffle "deal with increment 7\ndeal with increment 9\ncut -2" (10,2,3)
-	testShuffle "deal into new stack\ncut -2\ndeal with increment 7\ncut 8\ncut -4\ndeal with increment 7\ncut 3\ndeal with increment 9\ndeal with increment 3\ncut -1" (10,7,7)
+	testShuffle "deal with increment 7\ndeal into new stack\ndeal into new stack" 10 (0,7)
+	testShuffle "cut 6\ndeal with increment 7\ndeal into new stack" 10 (1,3)
+	testShuffle "deal with increment 7\ndeal with increment 9\ncut -2" 10 (2,3)
+	testShuffle "deal into new stack\ncut -2\ndeal with increment 7\ncut 8\ncut -4\ndeal with increment 7\ncut 3\ndeal with increment 9\ndeal with increment 3\ncut -1" 10 (7,7)
 	where
-		testShuffle s deck@(size,_,_) = test $ doOperations (parseInput s) (newDeck size) == deck
+		testShuffle s size res = test $ (combineOperations size $ parseInput s) == res
 
 main :: IO ()
 main = do
 	ops <- getInput
-	let deckA = doOperations ops $ newDeck 10007
-	print $ cardPosition deckA 2019
-	let deckB = doOperations ops $ newDeck 119315717514047
-	let repDeckB = multiplyOperations deckB 101741582076661
-	print $ cardAt repDeckB 2020
+	let partA = combineOperations 10007 ops
+	print $ cardPosition 10007 partA 2019
+	let partB = combineOperations 119315717514047 ops
+	let repPartB = multiplyOperations 119315717514047 partB 101741582076661
+	print $ cardAt 119315717514047 repPartB 2020
