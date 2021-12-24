4437bf36a890850b970911ccc652ff068310f097
phlip <git@mrphlip.com>
2021-12-24 17:25:50 +1100

    Puzzle 24

---
+++ 2021/24.py
@@ -0,0 +1,45 @@
+dat = [
+	("inp", 'w',),("mul", 'x', 0),("add", 'x', 'z'),("mod", 'x', 26),("div", 'z', 1),("add", 'x', 14),("eql", 'x', 'w'),("eql", 'x', 0),("mul", 'y', 0),("add", 'y', 25),("mul", 'y', 'x'),("add", 'y', 1),("mul", 'z', 'y'),("mul", 'y', 0),("add", 'y', 'w'),("add", 'y', 16),("mul", 'y', 'x'),("add", 'z', 'y'),("inp", 'w',),("mul", 'x', 0),("add", 'x', 'z'),("mod", 'x', 26),("div", 'z', 1),("add", 'x', 11),("eql", 'x', 'w'),("eql", 'x', 0),("mul", 'y', 0),("add", 'y', 25),("mul", 'y', 'x'),("add", 'y', 1),("mul", 'z', 'y'),("mul", 'y', 0),("add", 'y', 'w'),("add", 'y', 3),("mul", 'y', 'x'),("add", 'z', 'y'),("inp", 'w',),("mul", 'x', 0),("add", 'x', 'z'),("mod", 'x', 26),("div", 'z', 1),("add", 'x', 12),("eql", 'x', 'w'),("eql", 'x', 0),("mul", 'y', 0),("add", 'y', 25),("mul", 'y', 'x'),("add", 'y', 1),("mul", 'z', 'y'),("mul", 'y', 0),("add", 'y', 'w'),("add", 'y', 2),("mul", 'y', 'x'),("add", 'z', 'y'),("inp", 'w',),("mul", 'x', 0),("add", 'x', 'z'),("mod", 'x', 26),("div", 'z', 1),("add", 'x', 11),("eql", 'x', 'w'),("eql", 'x', 0),("mul", 'y', 0),("add", 'y', 25),("mul", 'y', 'x'),("add", 'y', 1),("mul", 'z', 'y'),("mul", 'y', 0),("add", 'y', 'w'),("add", 'y', 7),("mul", 'y', 'x'),("add", 'z', 'y'),("inp", 'w',),("mul", 'x', 0),("add", 'x', 'z'),("mod", 'x', 26),("div", 'z', 26),("add", 'x', -10),("eql", 'x', 'w'),("eql", 'x', 0),("mul", 'y', 0),("add", 'y', 25),("mul", 'y', 'x'),("add", 'y', 1),("mul", 'z', 'y'),("mul", 'y', 0),("add", 'y', 'w'),("add", 'y', 13),("mul", 'y', 'x'),("add", 'z', 'y'),("inp", 'w',),("mul", 'x', 0),("add", 'x', 'z'),("mod", 'x', 26),("div", 'z', 1),("add", 'x', 15),("eql", 'x', 'w'),("eql", 'x', 0),("mul", 'y', 0),("add", 'y', 25),("mul", 'y', 'x'),("add", 'y', 1),("mul", 'z', 'y'),("mul", 'y', 0),("add", 'y', 'w'),("add", 'y', 6),("mul", 'y', 'x'),("add", 'z', 'y'),("inp", 'w',),("mul", 'x', 0),("add", 'x', 'z'),("mod", 'x', 26),("div", 'z', 26),("add", 'x', -14),("eql", 'x', 'w'),("eql", 'x', 0),("mul", 'y', 0),("add", 'y', 25),("mul", 'y', 'x'),("add", 'y', 1),("mul", 'z', 'y'),("mul", 'y', 0),("add", 'y', 'w'),("add", 'y', 10),("mul", 'y', 'x'),("add", 'z', 'y'),("inp", 'w',),("mul", 'x', 0),("add", 'x', 'z'),("mod", 'x', 26),("div", 'z', 1),("add", 'x', 10),("eql", 'x', 'w'),("eql", 'x', 0),("mul", 'y', 0),("add", 'y', 25),("mul", 'y', 'x'),("add", 'y', 1),("mul", 'z', 'y'),("mul", 'y', 0),("add", 'y', 'w'),("add", 'y', 11),("mul", 'y', 'x'),("add", 'z', 'y'),("inp", 'w',),("mul", 'x', 0),("add", 'x', 'z'),("mod", 'x', 26),("div", 'z', 26),("add", 'x', -4),("eql", 'x', 'w'),("eql", 'x', 0),("mul", 'y', 0),("add", 'y', 25),("mul", 'y', 'x'),("add", 'y', 1),("mul", 'z', 'y'),("mul", 'y', 0),("add", 'y', 'w'),("add", 'y', 6),("mul", 'y', 'x'),("add", 'z', 'y'),("inp", 'w',),("mul", 'x', 0),("add", 'x', 'z'),("mod", 'x', 26),("div", 'z', 26),("add", 'x', -3),("eql", 'x', 'w'),("eql", 'x', 0),("mul", 'y', 0),("add", 'y', 25),("mul", 'y', 'x'),("add", 'y', 1),("mul", 'z', 'y'),("mul", 'y', 0),("add", 'y', 'w'),("add", 'y', 5),("mul", 'y', 'x'),("add", 'z', 'y'),("inp", 'w',),("mul", 'x', 0),("add", 'x', 'z'),("mod", 'x', 26),("div", 'z', 1),("add", 'x', 13),("eql", 'x', 'w'),("eql", 'x', 0),("mul", 'y', 0),("add", 'y', 25),("mul", 'y', 'x'),("add", 'y', 1),("mul", 'z', 'y'),("mul", 'y', 0),("add", 'y', 'w'),("add", 'y', 11),("mul", 'y', 'x'),("add", 'z', 'y'),("inp", 'w',),("mul", 'x', 0),("add", 'x', 'z'),("mod", 'x', 26),("div", 'z', 26),("add", 'x', -3),("eql", 'x', 'w'),("eql", 'x', 0),("mul", 'y', 0),("add", 'y', 25),("mul", 'y', 'x'),("add", 'y', 1),("mul", 'z', 'y'),("mul", 'y', 0),("add", 'y', 'w'),("add", 'y', 4),("mul", 'y', 'x'),("add", 'z', 'y'),("inp", 'w',),("mul", 'x', 0),("add", 'x', 'z'),("mod", 'x', 26),("div", 'z', 26),("add", 'x', -9),("eql", 'x', 'w'),("eql", 'x', 0),("mul", 'y', 0),("add", 'y', 25),("mul", 'y', 'x'),("add", 'y', 1),("mul", 'z', 'y'),("mul", 'y', 0),("add", 'y', 'w'),("add", 'y', 4),("mul", 'y', 'x'),("add", 'z', 'y'),("inp", 'w',),("mul", 'x', 0),("add", 'x', 'z'),("mod", 'x', 26),("div", 'z', 26),("add", 'x', -12),("eql", 'x', 'w'),("eql", 'x', 0),("mul", 'y', 0),("add", 'y', 25),("mul", 'y', 'x'),("add", 'y', 1),("mul", 'z', 'y'),("mul", 'y', 0),("add", 'y', 'w'),("add", 'y', 6),("mul", 'y', 'x'),("add", 'z', 'y'),
+]
+
+def dm(a,b):
+	d = a // b
+	m = a % b
+	if d < 0 and m != 0:
+		d += 1
+		m -= b
+	return d,m
+
+def run(input):
+	vars = {k:0 for k in "wxyz"}
+	def p(x):
+		if isinstance(x, int):
+			return x
+		else:
+			return vars[x]
+	for opcode, *params in dat:
+		if opcode == "inp":
+			vars[params[0]] = input.pop(0)
+		elif opcode == "add":
+			vars[params[0]] += p(params[1])
+		elif opcode == "mul":
+			vars[params[0]] *= p(params[1])
+		elif opcode == "div":
+			if p(params[1]) == 0:
+				raise "div by zero"
+			vars[params[0]] = dm(p(params[0]), p(params[1]))[0]
+		elif opcode == "mod":
+			if p(params[1]) <= 0 or p(params[0]) < 0:
+				raise "mod by invalid args"
+			vars[params[0]] = dm(p(params[0]), p(params[1]))[1]
+		elif opcode == "eql":
+			vars[params[0]] = 1 if p(params[0]) == p(params[1]) else 0
+	return vars['z'] == 0
+
+#from itertools import product
+#for inp in product("987654321", repeat=14):
+#	if run([int(i) for i in inp]):
+#		print(''.join(inp))
+#		break
+print(run([int(i) for i in "59996912981939"]))
+print(run([int(i) for i in "17241911811915"]))
